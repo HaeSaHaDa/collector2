418e2c1d147a7a2d4e59e1494c6d26a951e5d4a9
sixthgkrsus <sixthgkrsus@naver.com>
2024-11-07 16:18:17 +0900

    예외처리 간소화, 엑셀 생성부분 간소화 할 예정

---
+++ src/main/java/solomonm/ugo/collector/dbtoexcel/dto/MailDTO.java
@@ -0,0 +1,37 @@
+package solomonm.ugo.collector.dbtoexcel.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import solomonm.ugo.collector.dbtoexcel.config.MessageConstant;
+
+import java.util.List;
+
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MailDTO {
+    @NotBlank
+    @Email(message = "Sender" + MessageConstant.VALID_ERROR)
+    private String sender;
+
+    @NotBlank
+    @Email(message = "Recipient" + MessageConstant.VALID_ERROR)
+    private List<Object> recipients;
+
+    @NotBlank(message = "Title" + MessageConstant.BLANK_ERROR)
+    private String title;
+
+    @NotBlank(message = "Content" + MessageConstant.BLANK_ERROR)
+    private String content;
+
+    @NotBlank(message = "Delivery type" + MessageConstant.BLANK_ERROR)
+    private String deliveryType;
+}
